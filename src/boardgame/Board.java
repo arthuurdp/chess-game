01342a1bc11d0945c0cc4ff06e5d1e86e96c9b8d
Arthur Dall Agnol Pinheiro <arthurdallagnol0702@gmail.com>
2025-12-26 11:28:45 -0300

    board methods implemented

---
+++ src/boardgame/Board.java
@@ -0,0 +1,58 @@
+package boardgame;
+
+public class Board {
+    private Integer rows;
+    private Integer columns;
+    private Piece[][] pieces;
+
+    public Board() {
+    }
+
+    public Board(Integer rows, Integer columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public Integer getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Integer columns) {
+        this.columns = columns;
+    }
+
+    public Piece piece(Integer row, Integer column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+
+    private boolean positionExists(Integer row, Integer column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        return piece(position) != null;
+    }
+
+}
